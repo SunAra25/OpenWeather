93d6f66bc5ebb858f4bb41f10eef7a5ace0ef20d
SunAra25 <tjsdkfk77@naver.com>
2024-07-10 15:26:28 +0900

    [Chore]  프로젝트 초기 세팅 및 Dependency 추가

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by 아라 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
